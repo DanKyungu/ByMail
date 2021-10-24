a1e26984ff9c3763c8a64a3c46eff6863e47d6e8
Eliezer BWANA <44249870+Eel2000@users.noreply.github.com>
2021-10-24 17:32:44 +0200

    🚧complete menu

--- ByMail/ViewModels/MainWindowViewModel.cs
+++ ByMail/ViewModels/MainWindowViewModel.cs
@@ -1,14 +1,89 @@
-using System;
-using System.Collections.Generic;
-using System.Text;
-
 namespace ByMail.ViewModels
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Collections.ObjectModel;
+    using System.Text;
+    using ByMail.Models;
+    using ReactiveUI;
+
+#nullable disable
+
     /// <summary>
     /// Interaction logic for MainWindow.axaml
     /// </summary>
     public class MainWindowViewModel : ViewModelBase
     {
-        public string Greeting => "Welcome to Avalonia!";
+        private ObservableCollection<MenuItem> menuItems;
+        private ObservableCollection<Folder> folders;
+
+        public ObservableCollection<Folder> Folders
+        {
+            get => this.folders;
+            set => this.RaiseAndSetIfChanged(ref this.folders, value);
+        }
+        public ObservableCollection<MenuItem> MenuItems
+        {
+            get => this.menuItems;
+            set => this.RaiseAndSetIfChanged(ref this.menuItems, value);
+        }
+
+        public MainWindowViewModel()
+        {
+            LoadSideBarData();
+        }
+
+        protected void LoadSideBarData()
+        {
+            MenuItems = new ObservableCollection<MenuItem>()
+            {
+                new MenuItem
+                {
+                    Title="Inbox",
+                    Count="12",
+                    Path = "InboxPagePath",
+                    Icon="PutAvaloniaIconForMenu"
+                },
+                new MenuItem
+                {
+                    Title="Starred",
+                    Path="StarredPagePath",
+                    Icon="IconForStarred"
+                },
+                new MenuItem
+                {
+                    Title="Sent",
+                    Path="SentMailsPath",
+                    Icon="IconForSent"
+                },
+                new MenuItem
+                {
+                    Title="Drafts",
+                    Path="DraftsPagePath",
+                    Icon="IconForDraft"
+                },
+                new MenuItem
+                {
+                    Title="Deleted",
+                    Path="DeletedPagePath",
+                    Icon="IconForDeleted"
+                },
+                new MenuItem
+                {
+                    Title="Spam",
+                    Path="SpamPagePath",
+                    Icon="IconForSpam"
+                }
+            };
+
+            Folders = new ObservableCollection<Folder>()
+            {
+                new Folder
+                {
+                    Name="Work folder",
+                    Path=@"C:"
+                }
+            };
+        }
     }
 }
