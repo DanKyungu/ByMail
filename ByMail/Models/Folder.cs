6759519f7e29b4dd06f625bbc9f98de0531cec37
Eliezer BWANA <44249870+Eel2000@users.noreply.github.com>
2021-10-27 00:40:11 +0200

    🚧custom account image

---
+++ ByMail/Models/Folder.cs
@@ -0,0 +1,15 @@
+﻿namespace ByMail.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+#nullable disable
+
+    public class Folder
+    {
+        public string Name { get; set; }
+        public string Path { get; set; }
+    }
+}
---
+++ ByMail/Models/MenuItem.cs
@@ -0,0 +1,18 @@
+﻿namespace ByMail.Models
+{
+    using Avalonia.Media;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+#nullable disable
+    public class MenuItem
+    {
+        public string Title { get; set; }
+        public string Count { get; set; }
+        public string Path { get; set; }
+        public string Icon { get; set; }
+        public Geometry Ico { get; set; }
+    }
+}
